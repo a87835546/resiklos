141354a77b0f18fa2fd7db0c70723e662173068e
yicen <a87835546@gmail.com>
2022-03-29 23:13:58 +0800

    update:my wallet page

--- lib/wallet/my_wallet_page.dart
+++ lib/wallet/my_wallet_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:resiklos/base_class/base_page.dart';
 import 'package:resiklos/home/transactions/transaction_segment_view.dart';
+import 'package:resiklos/utils/app_singleton.dart';
 import 'package:resiklos/utils/color.dart';
 import 'package:resiklos/wallet/my_wallet_activity_page.dart';
 import 'package:resiklos/wallet/my_wallet_assets_page.dart';
@@ -11,6 +12,7 @@ import 'package:resiklos/wallet/my_wallet_button_widget.dart';
 
 class MyWalletPage extends BaseStatefulWidget {
   const MyWalletPage({Key? key}) : super(key: key);
+
   @override
   BaseStatefulState<BaseStatefulWidget> getState() => _MyWalletPageState();
 }
@@ -23,6 +25,11 @@ class _MyWalletPageState extends BaseStatefulState<MyWalletPage> {
   ];
   List pages = const [MyWalletAssetsPage(), MyWalletActivityPage()];
   int _index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +52,7 @@ class _MyWalletPageState extends BaseStatefulState<MyWalletPage> {
                     height: 5,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(2.5),
-                        color:const Color(0xff75BE00)),
+                        color: const Color(0xff75BE00)),
                   ),
                   Expanded(
                     child: Container(
@@ -68,22 +75,139 @@ class _MyWalletPageState extends BaseStatefulState<MyWalletPage> {
               ),
             ),
             Padding(
-              padding:const EdgeInsets.only(left: 15, right: 15),
+              padding: const EdgeInsets.only(left: 15, right: 15),
               child: Container(
-                height: 180,
-                decoration: BoxDecoration(
-                  color: Colors.yellowAccent,
-                  borderRadius: BorderRadius.circular(5),
-                  boxShadow: const [
-                    BoxShadow(
-                        color: Color(0x29000000),
-                        blurRadius: 6.0,
-                        offset: Offset(0, 2),
-                        spreadRadius: 0)
-                  ],
-                ),
-                child: Container(),
-              ),
+                  height: 180,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    color: mainColor(),
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Color(0x29000000),
+                          blurRadius: 6.0,
+                          offset: Offset(0, 2),
+                          spreadRadius: 0)
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(left: 12, right: 12),
+                        color: Colors.redAccent,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 40,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 20,
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      "RSG Wallet",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 20,
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "Resiklos Gems",
+                                      style: TextStyle(
+                                          color: Colors.white.withOpacity(0.6),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 10),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                                child: Container(
+                              height: 40,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: Text(
+                                      "RP:${AppSingleton.userInfoModel?.rpWalletAddress}",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12),
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(left: 5, right: 5),
+                                    child: Icon(
+                                      Icons.copy,
+                                      color: mainColor(),
+                                      size: 12,
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.only(left: 0, right: 5),
+                                    child: Icon(
+                                      Icons.share,
+                                      color: mainColor(),
+                                      size: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ))
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          height: 40,
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width - 30,
+                          child: const Text(
+                            "500 RSG",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 34,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        left: 0,
+                        top: 70,
+                      ),
+                      Positioned(
+                        child: Container(
+                          alignment: Alignment.center,
+                          clipBehavior: Clip.antiAlias,
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.white),
+                          child: Container(
+                            width: 60,
+                            height: 60,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.transparent),
+                            child: Image.asset(
+                              "imgs/logo@2x.png",
+                              width: 60,
+                              height: 60,
+                            ),
+                          ),
+                        ),
+                        left: (MediaQuery.of(context).size.width - 110) / 2,
+                        top: 0,
+                      )
+                    ],
+                  )),
             ),
             Padding(
               padding: const EdgeInsets.only(left: 40, top: 20, right: 40),
